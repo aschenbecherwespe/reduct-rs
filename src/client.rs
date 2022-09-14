3725053d4910cf2992636dae782d88dc9eb2ac76
aschenbecherwespe <ciaran.moyne@gmail.com>
2022-09-14 22:06:54 +0200

    finally a half-working info :D

--- src/client.rs
+++ src/client.rs
@@ -1,20 +1,32 @@
-use url::{ Url };
 use hyper::Client as HyperClient;
-use tokio::io::{self, AsyncWriteExt as _};
+use hyper::body::HttpBody as _;
+use hyper::Uri;
+use std::str;
 
 pub struct Client {
-    url: Url
+    url: Uri
 }
 
 impl Client {
-    async fn info(self) {
-        let client = HyperClient::new();
-        let res = client.get(self.url).await?;
+    async fn info(self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let http_client = HyperClient::new();
+        let mut res = http_client.get(self.url).await?;
+        let mut body = "".to_owned();
         while let Some(next) = res.data().await {
             let chunk = next?;
-            io::stdout().write_all(&chunk).await?;
+            body.push_str(str::from_utf8(&chunk)?);
         }
+        println!("{}", body);
+        Ok(())
     }
 }
 
-
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn test_info() {
+        let client = Client { url: Uri::from_static("http://localhost:8383/info") };
+        let _res = client.info().await;
+    }
+}
